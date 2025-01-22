6dcb7dd9873d861620fe59f3a6a213da0fca3920
phuvu <phu270904@gmail.com>
2025-01-22 23:13:42 +0700

    feat/set up main.ts api-gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      port: process.env.PORT || 3001,
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`API Gateway is running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
