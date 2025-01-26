900fb337b4223e1b048d5d6876629aafb4b39ded
phuvu <phu270904@gmail.com>
2025-01-26 14:50:58 +0700

    feat/user entity

---
+++ account/src/users/entities/user.entity.ts
@@ -0,0 +1,82 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 50 })
+  @IsString()
+  first_name: string;
+
+  @Column({ length: 50 })
+  @IsString()
+  last_name: string;
+
+  @Column()
+  @IsOptional()
+  gender?: Gender;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column()
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @Column()
+  @IsPhoneNumber()
+  phone_number: string;
+
+  @Column()
+  @IsOptional()
+  address?: string;
+
+  @Column()
+  @IsOptional()
+  city?: string;
+
+  @Column()
+  @IsOptional()
+  country?: string;
+
+  @Column()
+  @IsString()
+  role: string;
+
+  @Column()
+  @IsOptional()
+  avatar?: string;
+
+  @Column()
+  @IsOptional()
+  @IsString()
+  refresh_token?: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
