4e041838a4eb4f1c8eb21add93673dd8cdcbcc00
phuvu <phu270904@gmail.com>
2025-01-27 18:35:55 +0700

    feat/MySql db connected

--- account/src/ormconfig.ts
+++ account/src/ormconfig.ts
@@ -10,7 +10,7 @@ export default {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [`${__dirname}/../**/*.entity.{js,ts}`],
+  entities: [`dist/**/*.entity.{js,ts}`],
   synchronize: false,
   migrations: ['src/migations/*.ts', 'dist/migrations/*.js'],
   cli: {
