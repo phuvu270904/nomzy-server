69d86b6e03f7530036965cf4ee0cece995169dd6
phuvu <phu270904@gmail.com>
2025-06-01 12:57:08 +0700

    feat: update user profile

--- src/api/users/dto/updateUser.dto.ts
+++ src/api/users/dto/updateUser.dto.ts
@@ -1,47 +1,67 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    example: 'John',
+    description: 'The first name of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   first_name?: string;
 
+  @ApiProperty({
+    example: 'Doe',
+    description: 'The last name of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   last_name?: string;
 
-  @IsEmail()
-  @IsOptional()
-  email?: string;
-
-  @IsString()
-  @IsOptional()
-  password?: string;
-
+  @ApiProperty({
+    example: '0938123456',
+    description: 'The phone number of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   phone_number?: string;
 
+  @ApiProperty({
+    example: '123 Main St',
+    description: 'The address of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   address?: string;
 
+  @ApiProperty({
+    example: 'New York',
+    description: 'The city of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   city?: string;
 
+  @ApiProperty({
+    example: 'NY',
+    description: 'The state of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   country?: string;
 
+  @ApiProperty({
+    example: 'https://example.com/avatar.jpg',
+    description: 'The avatar URL of the user',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   avatar?: string;
-
-  @IsString()
-  @IsOptional()
-  refresh_token?: string;
-
-  @IsString()
-  @IsOptional()
-  resetToken?: string;
 }
