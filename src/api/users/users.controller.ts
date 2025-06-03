60a960fe0d6f791bc46ef3419ae8544189e4554f
phuvu <phu270904@gmail.com>
2025-06-03 13:34:13 +0700

    feat: admin role for user apis

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -11,21 +11,28 @@ import { UsersService } from './users.service';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/role.enum';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiBearerAuth('access-token')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(Role.ADMIN)
   @Get()
   async findAll(): Promise<UserEntity[]> {
     return this.usersService.findAll();
   }
 
+  @Roles(Role.ADMIN)
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<UserEntity | null> {
     return this.usersService.findOne(id);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<any> {
     const user = await this.usersService.create(createUserDto);
@@ -33,6 +40,7 @@ export class UsersController {
     return result;
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   async update(
     @Param('id') id: number,
@@ -41,6 +49,7 @@ export class UsersController {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   async remove(@Param('id') id: number): Promise<void> {
     return this.usersService.remove(id);
