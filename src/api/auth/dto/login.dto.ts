a485508df418b3680bbe50eb1c6263763836225b
phuvu <phu270904@gmail.com>
2025-06-12 22:56:28 +0700

    refactor: lint code

--- src/api/auth/dto/login.dto.ts
+++ src/api/auth/dto/login.dto.ts
@@ -2,12 +2,20 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, Min, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'admin@gmail.com', description: 'Email', required: true  })
+  @ApiProperty({
+    example: 'admin@gmail.com',
+    description: 'Email',
+    required: true,
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty({ example: 'Abcd1234!', description: 'Password', required: true })
+  @ApiProperty({
+    example: 'Abcd1234!',
+    description: 'Password',
+    required: true,
+  })
   @IsString()
   @MinLength(8)
   @IsNotEmpty()
