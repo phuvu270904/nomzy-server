b358c88e3b1c03d8dab3f63fda38e617ce63c021
phuvu <phu270904@gmail.com>
2025-11-21 02:16:51 +0700

    feat: remove UserNotificationsModule from NotificationSentModule imports

--- src/api/notification-sent/notification-sent.module.ts
+++ src/api/notification-sent/notification-sent.module.ts
@@ -5,14 +5,12 @@ import { NotificationSentService } from './notification-sent.service';
 import { NotificationSentEntity } from './entities/notification-sent.entity';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { UsersModule } from '../users/users.module';
-import { UserNotificationsModule } from '../user-notifications/user-notifications.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([NotificationSentEntity]),
     NotificationsModule,
     UsersModule,
-    UserNotificationsModule,
   ],
   controllers: [NotificationSentController],
   providers: [NotificationSentService],
